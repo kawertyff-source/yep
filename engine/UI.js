0d1a87f36f6b8066d661c35be2c11fdae16369de
kawertyff-source <kawertyff@gmail.com>
2026-02-18 08:57:35 +0700

    Update UI.js

--- engine/UI.js
+++ engine/UI.js
@@ -3,26 +3,55 @@ export default class UI {
   constructor(game){
     this.game=game;
 
-    document.querySelector(".light").ontouchstart=()=>this.light();
-    document.querySelector(".heavy").ontouchstart=()=>this.heavy();
-    document.querySelector(".dash").ontouchstart=()=>this.dash();
-    document.querySelector(".block").ontouchstart=()=>this.block();
+    this.setupButtons();
+    this.setupJoystick();
   }
 
-  light(){
-    console.log("Light Attack");
+  setupButtons(){
+
+    document.querySelector(".light").ontouchstart=()=>{
+      this.game.player.attack(this.game.enemy,5);
+    };
+
+    document.querySelector(".heavy").ontouchstart=()=>{
+      this.game.player.attack(this.game.enemy,15);
+    };
+
+    document.querySelector(".dash").ontouchstart=()=>{
+      this.game.player.dash(1);
+    };
+
+    document.querySelector(".block").ontouchstart=()=>{
+      this.game.player.blocking=true;
+    };
+
+    document.querySelector(".block").ontouchend=()=>{
+      this.game.player.blocking=false;
+    };
   }
 
-  heavy(){
-    console.log("Heavy Attack");
-  }
+  setupJoystick(){
 
-  dash(){
-    console.log("Dash");
-  }
+    const joystick=document.querySelector(".joystick");
+    const stick=document.querySelector(".stick");
 
-  block(){
-    console.log("Block");
+    joystick.ontouchmove=(e)=>{
+      const rect=joystick.getBoundingClientRect();
+      const touch=e.touches[0];
+
+      const x=touch.clientX-rect.left-70;
+      const y=touch.clientY-rect.top-70;
+
+      stick.style.left=(x+40)+"px";
+      stick.style.top=(y+40)+"px";
+
+      this.game.player.move(x/50,y/50);
+    };
+
+    joystick.ontouchend=()=>{
+      stick.style.left="40px";
+      stick.style.top="40px";
+    };
   }
 
 }
